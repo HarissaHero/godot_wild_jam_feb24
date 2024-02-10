6f4ce762ab0e6637c8bb7dfc5278d82e265288a1
HarissaHero <anis.chd@proton.me>
2024-02-10 12:15:18 +0100

    feat: add day/night cycle

--- Game.gd
+++ Game.gd
@@ -1,11 +1,30 @@
 extends Node2D
 
+enum TimeCycle {
+  DAY = 0,
+  NIGHT = 1
+}
+
+var actualTimeCycle = TimeCycle.DAY
+
+@onready var dayNightCycleTimer : Timer = $DayNightCycleTimer
+@onready var dayNightCycleIndicatorLabel: Label = $HUD/DayNightCycleIndicator
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  dayNightCycleTimer.start()
+  update_day_night_indicator()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
   pass
+
+
+func _on_day_night_cycle_timer_timeout():
+  actualTimeCycle = ((actualTimeCycle + 1) % 2) as TimeCycle
+  update_day_night_indicator()
+
+
+func update_day_night_indicator(): 
+  dayNightCycleIndicatorLabel.text = "Day" if actualTimeCycle == TimeCycle.DAY else "Night"
