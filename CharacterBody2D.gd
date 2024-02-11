1ed380ab7bd6923a686291ff7ccb5e6965f49371
HarissaHero <anis.chd@proton.me>
2024-02-11 17:28:37 +0100

    feat: add bonus mechanic and score

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -8,13 +8,18 @@ signal playerDeadSignal
 @onready var energyConsumptionTimer = $EnergyConsumptionTimer
 @onready var energyHUDBar: ProgressBar = $Camera2D/HUD/ProgressBar
 @onready var gameOverLabel: Label = $Camera2D/HUD/GameOver
+@onready var scoreLabel: Label = $Camera2D/HUD/Score
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var comboBonusTimer: Timer = $ComboBonusTimer
 
-const SPEED = 300.0
+var SPEED = 300.0
 
 var energy: int = maxEnergy  
 
 var invincibility = invincibilityDelay 
+
+var combo = 1 
+var score = 0
 
 func _ready():
   energyConsumptionTimer.start()
@@ -46,6 +51,8 @@ func _process(delta):
     # play dead animation 
     playerDeadSignal.emit() 
     gameOverLabel.visible = true
+    energyConsumptionTimer.stop()
+    comboBonusTimer.stop()
 
   if invincibility > 0:
     invincibility -= delta
@@ -53,13 +60,36 @@ func _process(delta):
 
 func _on_energy_consumption_timer_timeout():
   energy -= 1
+  score += 1
+  update_score_label()
 
 
 func _on_enemy_detector_body_entered(body:Node2D):
   if body.enemyStatus == ValueObjects.EnemyStatus.WEAK:
     body.queue_free()
-    energy += 1
+    if combo > 1:
+      SPEED = 600 
+    combo += 1
+    energy += 1 
+    if energy > 100:
+      energy = 100
+    score += combo
+    comboBonusTimer.start(-1)
+    update_score_label()
   elif body.enemyStatus == ValueObjects.EnemyStatus.STRONG:
     energy -= 1
+    reset_combo_speed()
     invincibility = invincibilityDelay 
 
+
+func _on_combo_bonus_timer_timeout():
+  reset_combo_speed()
+
+
+func reset_combo_speed():
+  combo = 1 
+  SPEED = 300
+
+
+func update_score_label():
+  scoreLabel.text = "Score: {score}".format({"score": score})
